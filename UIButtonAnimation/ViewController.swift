4d99a6a3c60624a6932371ec972ee37b7ab1fdc8
Kriti Agarwal <kriti.agarwal4709@gmail.com>
2020-08-10 17:08:45 +0530

    Initial Commit

---
+++ UIButtonAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIButtonAnimation
+//
+//  Created by Kriti Agarwal on 10/08/20.
+//  Copyright © 2020 KritiAgarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
